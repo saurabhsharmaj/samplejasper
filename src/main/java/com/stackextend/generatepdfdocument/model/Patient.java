3babcabf797832f76bc994415a5e6ba92493fe11
saurabhsharmaj <saurabh.find@gmail.com>
2019-10-28 17:03:19 +0530

    update

--- src/main/java/com/stackextend/generatepdfdocument/model/Patient.java
+++ src/main/java/com/stackextend/generatepdfdocument/model/Patient.java
@@ -3,26 +3,58 @@ package com.stackextend.generatepdfdocument.model;
 import lombok.Getter;
 import lombok.Setter;
 
+
 public class Patient {
-	@Getter 
-	@Setter 
+	 
 	private String name;
 	
-	@Getter 
-	@Setter 
 	private Integer age;
 	
-	@Getter 
-	@Setter 
+	
 	private String gender;
 	
-	@Getter 
-	@Setter 
 	private String information;
 	
-	@Getter 
-	@Setter 
 	private BasicHealth basicHealth;
 
-	
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Integer getAge() {
+		return age;
+	}
+
+	public void setAge(Integer age) {
+		this.age = age;
+	}
+
+	public String getGender() {
+		return gender;
+	}
+
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+
+	public String getInformation() {
+		return information;
+	}
+
+	public void setInformation(String information) {
+		this.information = information;
+	}
+
+	public BasicHealth getBasicHealth() {
+		return basicHealth;
+	}
+
+	public void setBasicHealth(BasicHealth basicHealth) {
+		this.basicHealth = basicHealth;
+	}
+
 }
